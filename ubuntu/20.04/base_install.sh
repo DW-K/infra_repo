570c1b0b7517f9079a1faa4f410113b501081ba9
DW-K <pch145@naver.com>
2023-02-08 19:09:44 +0900

    ubuntu

--- ubuntu/20.04/base_install.sh
+++ ubuntu/20.04/base_install.sh
@@ -1,6 +1,7 @@
-sudo ./sources.list /etc/apt/sources.list
-
 sudo apt update
 sudo apt-get update
 
-sudo apt-get install -y git
+sudo cp ./sources.list /etc/apt/sources.list
+
+sudo apt update
+sudo apt-get update
