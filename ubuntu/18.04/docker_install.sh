02cf07ce2a2d26378c60fb77d49ca6fd4400f734
DW-K <pch145@naver.com>
2023-02-09 02:12:03 +0900

    18.04

--- ubuntu/18.04/docker_install.sh
+++ ubuntu/18.04/docker_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install -y\
+sudo apt-get install -y \
    ca-certificates \
    curl \
    gnupg \
@@ -9,7 +9,7 @@ sudo apt-get install -y\
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # docker 자동 실행
 sudo systemctl start docker
