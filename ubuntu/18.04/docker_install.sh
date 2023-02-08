45462afea0e7ab8098d94f9e3002ced5f94583c0
DW-K <pch145@naver.com>
2023-02-08 21:28:12 +0900

    add docker-compose for test

--- ubuntu/18.04/docker_install.sh
+++ ubuntu/18.04/docker_install.sh
@@ -10,3 +10,13 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# docker 자동 실행
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+# docker 권한 설정
+sudo usermod -a -G docker $USER
+
+sudo service docker restart
