09d3c710ecb849bcd4cffc94a400a9fbe514863e
DW-K <pch145@naver.com>
2023-02-08 19:20:19 +0900

    ubuntu

--- ubuntu/20.04/base_install.sh
+++ ubuntu/20.04/base_install.sh
@@ -1,7 +1,6 @@
-sudo apt update
-sudo apt-get update
-
-sudo cp ./sources.list /etc/apt/sources.list
+sudo cp apt_repo/sources.list /etc/apt/sources.list
 
 sudo apt update
 sudo apt-get update
+
+sudo apt-get install vim
