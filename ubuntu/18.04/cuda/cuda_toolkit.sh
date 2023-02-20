ca3c584bb793eb5eb3405590d9192ae116f91105
DW-K <pch145@naver.com>
2023-02-20 14:44:12 +0900

    cuda

--- ubuntu/18.04/cuda/cuda_toolkit.sh
+++ ubuntu/18.04/cuda/cuda_toolkit.sh
@@ -1,25 +1,12 @@
-# 기존 cuda 제거
-
-sudo rm -rf /usr/local/cuda*
-
-export PATH=$PATH:/usr/local/cuda-11.0/bin
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.0/lib64
-
-export CUDADIR=/usr/local/cuda-11.0
 
 # 개발 필수 프로그램 설치
-sudo apt-get install build-essential 
+sudo apt-get install -y build-essential 
 
 # cuda toolkit 설치 (11.6.4)
 # https://developer.nvidia.com/cuda-toolkit-archive
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-sudo apt-get update
-sudo apt-get -y install cuda
+wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda_11.6.2_510.47.03_linux.run
+sudo sh cuda_11.6.2_510.47.03_linux.run
 
 # # 환경 변수 설정
 # sudo sh -c "echo 'export PATH=$PATH:/usr/local/cuda-11.2/bin' >> /etc/profile"
--- ubuntu/18.04/cuda/nvidia_driver.sh
+++ ubuntu/18.04/cuda/nvidia_driver.sh
@@ -1,15 +1,26 @@
 ubuntu-drivers devices
 
 # 0. 기존 cuda 삭제
-sudo apt-get purge nvidia* 
+sudo apt-get purge nvidia*
 sudo apt-get autoremove
 sudo apt-get autoclean
 sudo rm -rf /usr/local/cuda*
+
+# 기존 cuda 제거
+
+sudo rm -rf /usr/local/cuda*
+export PATH=$PATH:/usr/local/cuda-11.0/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.0/lib64
+export CUDADIR=/usr/local/cuda-11.0
 
 # 1. 권장드라이버 자동으로 설치
 sudo ubuntu-drivers autoinstall
 
 # 2. 원하는 버전 수동으로 설치
 # sudo apt install nvidia-driver-450
+
+sudo apt-get install dkms nvidia-modprobe
+sudo apt update
+sudo apt upgrade
 
 sudo reboot
